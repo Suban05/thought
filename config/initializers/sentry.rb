562e7528b4039a1f2487dc73260e9e01a3d29aa8
Suban05 <anatolyb94@gmail.com>
2024-11-09 20:37:43 +0500

    configure the sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,14 +1,6 @@
 Sentry.init do |config|
-  config.dsn = "https://examplePublicKey@o0.ingest.sentry.io/0"
-
-  # get breadcrumbs from logs
-  config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
-
-  # enable tracing
-  # we recommend adjusting this value in production
+  config.dsn = ENV['SENTRY_DSN']
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   config.traces_sample_rate = 1.0
-
-  # enable profiling
-  # this is relative to traces_sample_rate
   config.profiles_sample_rate = 1.0
 end
