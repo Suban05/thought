41e0658d150bd48b8bdadb44e7dbbb0465719d0e
Suban05 <anatolyb94@gmail.com>
2024-11-09 14:28:02 +0500

    fill categories by `seeds`

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+["Музыка", "Кино"].each do |category|
+  Category.create!(name: category)
+end
