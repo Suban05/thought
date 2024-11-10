1edce37653767062a08424a1e023301159f31808
Suban05 <anatolyb94@gmail.com>
2024-11-10 20:32:30 +0500

    validates posts and comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   belongs_to :creator, class_name: "User"
   belongs_to :category
   has_many :comments, class_name: "PostComment", dependent: :destroy
+  validates :title, :category, :body, presence: true
+  validates :title, length: { minimum: 5 }
 end
--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -2,4 +2,6 @@ class PostComment < ApplicationRecord
   belongs_to :post
   belongs_to :user
   has_ancestry
+
+  validates :content, presence: true
 end
