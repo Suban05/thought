4c0726f945879ed319a0ee3ed90a226ba25173db
Suban05 <anatolyb94@gmail.com>
2024-11-10 16:10:16 +0500

    destroy comments after post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :creator, class_name: "User"
   belongs_to :category
-  has_many :comments, class_name: "PostComment"
+  has_many :comments, class_name: "PostComment", dependent: :destroy
 end
