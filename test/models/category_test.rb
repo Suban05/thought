024914fba4b1248d3479987b1d3fee49b2afd492
Suban05 <anatolyb94@gmail.com>
2024-11-17 13:42:07 +0500

    The category should have a `name` field that is mandatory

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -3,7 +3,16 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @category = categories(:one)
+  end
+  test 'name should not be empty' do
+    @category.name = nil
+    assert_not @category.valid?
+  end
+
+  test 'name should be filled' do
+    @category.name = 'category'
+    assert @category.valid?
+  end
 end
